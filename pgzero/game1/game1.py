85c8cfc6b6af136190587642072838e97be2746b
Victor Bos <victor.bos@gmail.com>
2018-09-11 19:03:10 +0300

    Initial version

---
+++ pgzero/game1/game1.py
@@ -0,0 +1,28 @@
+alien = Actor('alien')
+alien.topright = 0, 10
+
+WIDTH = 400
+HEIGHT = 300
+
+def draw():
+    screen.fill((128, 128, 0))
+    alien.draw()
+
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+        alien.top += 40
+        if alien.top > HEIGHT:
+            alien.top = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        sounds.eep.play()
+        alien.image = 'alien_hurt'
+        alien.bottom += 20
+    else:
+        print('Ha ha, you missed!')
+        
+    
---
+++ pgzero/game1/game2.py
@@ -0,0 +1,33 @@
+alien = Actor('alien')
+alien.topright = 0, 10
+
+WIDTH = 400
+HEIGHT = 300
+
+def draw():
+    screen.fill((128, 128, 0))
+    alien.draw()
+
+
+def update():
+    alien.left += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+        alien.top += 40
+        if alien.top > HEIGHT:
+            alien.top = 0
+
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.eep.play()
+    alien.bottom += 20
+    clock.schedule_unique(set_alien_normal, 1.0)
+    
+def set_alien_normal():
+    alien.image = 'alien'
+    
+    
