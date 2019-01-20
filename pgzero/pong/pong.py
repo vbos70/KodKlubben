231899f158456fcb1450ace7b179efb6f685a6cf
Victor Bos <victor.bos@gmail.com>
2019-01-20 13:02:11 +0200

    Added 2 players and changed background to red.

--- pgzero/pong/pong.py
+++ pgzero/pong/pong.py
@@ -3,8 +3,20 @@
 WIDTH = 800
 HEIGHT = 500
 
+
+player1 = Actor('sprite1_magenta.png')
+player1.pos = (50, HEIGHT // 2)
+player2 = Actor('sprite1_purple.png')
+player2.pos = (WIDTH-50, HEIGHT // 2)
+
 def draw():
     
-    # a purple background color mixed as RED = 128, GREEN = 0, BLUE = 128 
-    screen.fill((128, 0, 128))
+    # a red background color mixed as RED = 128, GREEN = 0, BLUE = 0 
+    screen.fill((128, 0, 0))
+
+    # draw players
+    player1.draw()
+    player2.draw()
+    
+
     
