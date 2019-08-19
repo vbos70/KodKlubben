4be04a108ebf75c816ccb479641d95e284cd5b6a
Victor Bos <victor.bos@gmail.com>
2019-08-19 21:44:33 +0300

    Simple example series: example1.py created

---
+++ pgzero/example1.py
@@ -0,0 +1,12 @@
+# File: example1.py
+#
+# Set the window size:
+WIDTH = 800
+HEIGHT = 400
+
+# To run the example1.py (pgzero) game:
+#
+#   terminal: pgzrun example1.py
+#
+# From the Mu editor:
+#   Click "Play" button or hit F5
