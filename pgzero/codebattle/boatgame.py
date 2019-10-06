e7e6e792e35ffff15d7dd9c32ccc75a17ca8b715
Victor Bos <victor.bos@gmail.com>
2019-10-06 20:49:57 +0300

    Integrated the boatbattle with the pgzero boatgame.

--- pgzero/codebattle/boatgame.py
+++ pgzero/codebattle/boatgame.py
@@ -1,3 +1,5 @@
+from boatbattle import *
+
 board_size = 10
 
 def load_boat_images():
@@ -15,16 +17,23 @@ class BoatGame: pass
 boat_game = BoatGame()
 boat_game.boats = load_boat_images()
 
-boat_game.boat_1 = boat_game.boats["red"][0]
-boat_game.boat_2 = boat_game.boats["red"][1]
+boat_game.boat_1_imgs = boat_game.boats["red"]
+boat_game.boat_2_imgs = boat_game.boats["yellow"]
 
-CELL_SIZE = max(boat_game.boat_1.width, boat_game.boat_1.height) * 2
+boat_game.bot_1 = Bot()
+boat_game.bot_2 = Bot()
+
+boat_game.started = False
+
+boat_game.BB = BoatBattle(board_size, [boat_game.bot_1, boat_game.bot_2], max_turn = 1000)
+
+CELL_SIZE = max(boat_game.boat_1_imgs[0].width, boat_game.boat_1_imgs[0].height) * 2
 
 WIDTH  = CELL_SIZE * board_size
 HEIGHT = CELL_SIZE * board_size
 
-def cell_coords(col, row):
-
+def cell_coords(col_row):
+    col, row = col_row
     def bc(c):
         if c < 0:
             return 0
@@ -34,14 +43,34 @@ def cell_coords(col, row):
     
     return (bc(col) * CELL_SIZE + 0.5 * CELL_SIZE, bc(row) * CELL_SIZE + 0.5 * CELL_SIZE)
 
-print(boat_game.boat_2.width, boat_game.boat_2.height)
 
 def draw():
 
     screen.fill((100, 150, 200))
 
-    boat_game.boat_1.pos = cell_coords(0,0) 
-    boat_game.boat_1.draw()
-    boat_game.boat_2.pos = cell_coords(5,5)
-    boat_game.boat_2.draw()
+    boat_game.boat_1_imgs[0].draw()
+    boat_game.boat_2_imgs[0].draw()
+
+
+def do_game_turn():
+    print("turn", boat_game.BB.turn)
+    bb = boat_game.BB
+    if not bb.stop_game:
+        if bb.max_turn ==  0 or bb.max_turn > bb.turn:
+            bb.play_turn()
+            animate(boat_game.boat_1_imgs[0], pos=cell_coords(boat_game.BB.position[boat_game.bot_1]))
+            animate(boat_game.boat_2_imgs[0], pos=cell_coords(boat_game.BB.position[boat_game.bot_2]))
+            
+        else:
+            bb.stop_game = True
+
     
+def update():
+    if boat_game.BB.stop_game:
+        clock.unschedule(do_game_turn)
+    else:
+        if not boat_game.started:
+            boat_game.boat_1_imgs[0].pos = cell_coords(boat_game.BB.position[boat_game.bot_1])
+            boat_game.boat_2_imgs[0].pos = cell_coords(boat_game.BB.position[boat_game.bot_2])
+            boat_game.started = True
+            clock.schedule_interval(do_game_turn, 1)
