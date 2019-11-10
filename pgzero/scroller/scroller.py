74f162c3d73008ef701f118a4d2263e8f2ea6d69
Victor Bos <victor.bos@gmail.com>
2019-11-10 11:46:51 +0200

    Added game variables and a draw() function.

--- pgzero/scroller/scroller.py
+++ pgzero/scroller/scroller.py
@@ -1,9 +1,29 @@
 import pgzrun
 
+# Window size
+WIDTH = 800
+HEIGHT = 600
+
+# Game values stored in "Bertil"
+bertil = {
+    'x' : 0,              # horizontal position (pixels)
+    'y' : HEIGHT // 2,    # vertical position (pixels)
+    'r' : WIDTH // 10,    # circle radius (pixels)
+    'speed_x' : 10,       # horizontal speed (pixels/second)
+    'speed_y' : 0,        # vertical speed (pixels/second)
+}
+
 # Press ESC to quit
 def on_key_up(key):
     if key == keys.ESCAPE:
         exit()
+
+# draw the current scene
+def draw():
+    screen.draw.circle((bertil['x'], bertil['y']),
+                       bertil['r'],
+                       (255,0,0)
+    )
 
 # The last line starts pgzero:
 pgzrun.go()
