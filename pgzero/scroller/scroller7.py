5687cc22dfecbaf299f2762df8195f0ed716087e
Victor Bos <victor.bos@gmail.com>
2019-11-17 13:41:19 +0200

    Replaced dict item access by attribute access.

--- pgzero/scroller/scroller7.py
+++ pgzero/scroller/scroller7.py
@@ -32,15 +32,15 @@ def on_key_up(key):
 
     # UP-arrow moves circle up
     if key == keys.UP:
-        circle['speed_y'] = -100
+        circle.speed_y = -100
 
     # DOWN-arrow moves circle down
     elif key == keys.DOWN:
-        circle['speed_y'] = 100
+        circle.speed_y = 100
 
     # SPACE stops circle from moving up or down
     elif key == keys.SPACE:
-        circle['speed_y'] = 0
+        circle.speed_y = 0
         
 # draw the current scene
 def draw():
@@ -48,67 +48,67 @@ def draw():
     screen.fill((0, 0, 0))
 
     # draw the circle
-    screen.draw.circle((circle['x'], circle['y']),
-                       circle['r'],
+    screen.draw.circle((circle.x, circle.y),
+                       circle.r,
                        (255,0,0)
     )
 
     # draw the square
-    s = square['s']
-    r = Rect((square['x']-s, square['y']-s), (2*s, 2*s))
+    s = square.s
+    r = Rect((square.x-s, square.y-s), (2*s, 2*s))
     screen.draw.rect(r, (0,255,0))
 
 # update the current scene (60 times per second!)
 def update(dt):
 
     # compute new position of circle
-    circle['x'] += dt * circle['speed_x']
-    circle['y'] += dt * circle['speed_y']
+    circle.x += dt * circle.speed_x
+    circle.y += dt * circle.speed_y
 
     # if circle moves to the left of the screen, move it back
-    if circle['x'] < 0:
-        circle['x'] = WIDTH
+    if circle.x < 0:
+        circle.x = WIDTH
     
     # if circle moves to the right of the screen, move it back
-    if circle['x'] > WIDTH:
-        circle['x'] = 0
+    if circle.x > WIDTH:
+        circle.x = 0
     
     # if circle moves to the bottom of the screen, move it back
-    if circle['y'] > HEIGHT:
-        circle['y'] = 0
+    if circle.y > HEIGHT:
+        circle.y = 0
         
     # if circle moves to the top of the screen, move it back
-    if circle['y'] < 0:
-        circle['y'] = HEIGHT
+    if circle.y < 0:
+        circle.y = HEIGHT
 
     # compute new position of the square
-    square['x'] += dt * square['speed_x']
-    square['y'] += dt * square['speed_y']
+    square.x += dt * square.speed_x
+    square.y += dt * square.speed_y
 
     # if square moves to the left of the screen, move it back
-    if square['x'] < 0:
-        square['x'] = WIDTH
+    if square.x < 0:
+        square.x = WIDTH
 
     # if square moves to the right of the screen, move it back
-    if square['x'] > WIDTH:
-        square['x'] = 0
+    if square.x > WIDTH:
+        square.x = 0
     
     # if square moves to the top of the screen, move it back
-    if square['y'] < 0:
-        square['y'] = HEIGHT
+    if square.y < 0:
+        square.y = HEIGHT
         
     # if square moves to the bottom of the screen, move it back
-    if square['y'] > HEIGHT:
-        square['y'] = 0
+    if square.y > HEIGHT:
+        square.y = 0
 
 
-    if circle['x']+circle['r'] >= square['x']-square['s']:
-        if circle['y'] > square['y']-square['s']:
-            if circle['y'] < square['y']+square['s']:
+    if circle.x+circle.r >= square.x-square.s:
+        if circle.y > square.y-square.s:
+            if circle.y < square.y+square.s:
                 # collision!
 
-                circle['x'] = 0
-                square['x'] = WIDTH
+                circle.x = 0
+                square.x = WIDTH
                 
 # The last line starts pgzero:
 pgzrun.go()
